25ec8a246dcedb64f0af95c898b30bb96b5e8f1f
jw20152025-blip <jw20152025@gmail.com>
2026-08-12 14:17:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const session = require("express-session");
-const { createClient } = require("@supabase/supabase-js");
+
 
 const app = express();
 
