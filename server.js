407f2ef776d2f956e057926df3269636b31f5afa
jw20152025-blip <jw20152025@gmail.com>
2026-08-12 20:29:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const SUPABASE_SERVICE_ROLE_KEY =
 const SESSION_SECRET =
     process.env.SESSION_SECRET;
 
-const { createClient } = require("@supabase/supabase-js");
+
 
 if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
     console.error("❌ Missing Supabase environment variables.");
